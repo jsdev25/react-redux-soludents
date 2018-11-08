ac3bd7dee6a98b6b510324369d57793218108cca
YuKai <40814780+EugenePizzerbert@users.noreply.github.com>
2018-11-08 19:03:36 +0800

    top button up
    YD

--- frontend/src/components/welcome/index.js
+++ frontend/src/components/welcome/index.js
@@ -1,81 +1,79 @@
 import React from 'react';
-import {MainCarousel} from './maincarousel';
-import {Videoview} from './videoview';
-import {Autionview} from './autionview';
-import {Pricingitem} from './pricingitem';
-import {TeamMemberSlider} from './TeamMemberSlider';
-import {ContactView} from './ContactView';
+import { MainCarousel } from './maincarousel';
+import { Videoview } from './videoview';
+import { Autionview } from './autionview';
+import { Pricingitem } from './pricingitem';
+import { TeamMemberSlider } from './TeamMemberSlider';
+import { ContactView } from './ContactView';
 import { Footer } from './Footer';
+import { BackTop  } from 'antd';
 
-import {Row,Col} from 'antd';
+import { Row, Col } from 'antd';
 import './index.css';
 import { OurPartnerSlider } from './OurPartnerSlider';
 
 export default class Welcome extends React.Component {
   render() {
-    return(
-        <div className="container-fluid" id="home">
-          <MainCarousel/>
-          <Videoview />
-        
+    return (
+      <div className="container-fluid" id="home">
+        <BackTop>
+          <div className="ant-back-top-inner">Top</div>
+        </BackTop>
+        <MainCarousel />
+        <Videoview />
+
         <div id="choose">
-          <Autionview title='Choose your package' description='But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth'/>
+          <Autionview title='Choose your package' description='But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth' />
           <div className="price-view container">
-          <Row>
+            <Row>
               <Col xs={24} md={8}>
                 <center>
-                    <Pricingitem 
-                        title="Offer 1"
-                        price="24"
-                        products="Unlimited products"
-                        downloads="Unlimited downloads"
-                        capacity="1000 GB"
-                    />
+                  <Pricingitem
+                    title="Offer 1"
+                    price="24"
+                    products="Unlimited products"
+                    downloads="Unlimited downloads"
+                    capacity="1000 GB"
+                  />
                 </center>
               </Col>
               <Col xs={24} md={8}>
                 <center>
-                    <Pricingitem 
-                        title="Offer 2"
-                        price="39"
-                        products="Unlimited products"
-                        downloads="Unlimited downloads"
-                        capacity="1000 GB"
-                    />
+                  <Pricingitem
+                    title="Offer 2"
+                    price="39"
+                    products="Unlimited products"
+                    downloads="Unlimited downloads"
+                    capacity="1000 GB"
+                  />
                 </center>
               </Col>
               <Col xs={24} md={8}>
                 <center>
-                    <Pricingitem 
-                        title="Offer 3"
-                        price="45"
-                        products="Unlimited products"
-                        downloads="Unlimited downloads"
-                        capacity="1000 GB"
-                    />
-                    </center>
+                  <Pricingitem
+                    title="Offer 3"
+                    price="45"
+                    products="Unlimited products"
+                    downloads="Unlimited downloads"
+                    capacity="1000 GB"
+                  />
+                </center>
               </Col>
-          </Row>
-        </div>
+            </Row>
+          </div>
         </div>
 
         <div id="team">
-          <Autionview title='Our Team' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'/>
+          <Autionview title='Our Team' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt' />
         </div>
         <OurPartnerSlider />
         <TeamMemberSlider />
         <ContactView />
 
         <div id="contact">
-         <Footer/>
+          <Footer />
         </div>
 
-        
-
-        <div style={{bottom: 10, right: 10, position:'fixed'}}>
-          <a href="#home"><img src="https://www.qi-a.com/images/up-arrow-icon.png" alt="Smiley face" height="70" width="70"></img></a>
-        </div>
-        
       </div>
     );
   }
