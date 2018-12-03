5ef5bdd2606b4b20acc719df9776434b6f096946
sinstar2000 <strongweb102@gmail.com>
2018-12-03 13:37:03 +0530

    modified email settings.

--- backend/models/constants/email.js
+++ backend/models/constants/email.js
@@ -1,7 +1,7 @@
 const EMAIL_CONFIG = {
     username:'AKIAJAMZ7EB7FE2PD2PQ',
     password:'AuR6dmLvFF//ye3lLOX9YKmu+qvR49z+4cXRxvSud7px',
-    host:'smtp.us-west-2.amazonaws.com',
+    host:'email-smtp.us-west-2.amazonaws.com',
     port:587
 }
 
