80b318622f77c2e14560f13816efcb24736f619f
Ben Abitbol <Benjiab@hotmail.com>
2018-12-09 01:33:56 +0200

    fixing deploy

--- backend/models/constants/frontend.js
+++ backend/models/constants/frontend.js
@@ -1,10 +1,11 @@
-const FRONTEND_DEV_URLS = [ 'https://soludents.com' ];
+const FRONTEND_DEV_URLS = ["https://soludents.com"];
 
 const FRONTEND_PROD_URLS = [
-  'https://www.yourdomain.com',
-  'https://yourdomain.com'
+  "https://www.yourdomain.com",
+  "https://yourdomain.com"
 ];
 
-module.exports = process.env.NODE_ENV === 'production'
-  ? FRONTEND_PROD_URLS
-  : FRONTEND_DEV_URLS;
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? FRONTEND_PROD_URLS
+    : FRONTEND_DEV_URLS;
